d86423157291d4d137ac9edcbd683e4e0cc48465
Pooja0793 <72892373+Pooja0793@users.noreply.github.com>
2021-01-11 03:04:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+//Create variables here
+var dog, happyDog, database, foodS, foodStock;
+function preload()
+{
+  //load images here
+  hungryDog = loadImage("Dog.png");
+  fullDog = loadImage("happydog.png");
+}
+
+function setup() {
+  database = firebase.database();
+  createCanvas(500, 500);
+  dog = createSprite(250,250);
+  dog.addImage("hungry",hungryDog);
+  dog.addImage("full",fullDog);
+  dog.scale = 0.2
+
+  readData();
+  
+}
+
+
+function draw() {  
+  background(46,139,87);
+  fill("white")
+  textSize(25);
+  text("Food Remaining: "+foodS,180,150);
+  drawSprites();
+
+  //add styles here
+
+}
+
+function keyPressed(){
+  if(keyCode === UP_ARROW){
+    if(foodS>0){
+    database.ref("/").set({
+      Food : foodS-1
+    })
+    dog.changeImage("full")
+  }
+
+  }
+}
+ function readData(){
+  foodStock = database.ref("Food");
+  foodStock.on("value",function(data){
+    foodS = data.val();
+  });
+
+}
+
+
